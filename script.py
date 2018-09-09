b1aaf0d64a39ad41144a108374497a6b3db916d2
Chris Murfitt <chris@cmurfitt3.orangehome.co.uk>
2018-09-09 19:06:56 +0100

    Added some test items

---
+++ script.py
@@ -0,0 +1,3 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil","Cairo, Egypt"]
+
+test_traveller = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
