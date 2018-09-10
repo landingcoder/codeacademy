465d259ff4e82733a501aa2d6dd51fab1be8861f
landingcoder <themurfitts@gmail.com>
2018-09-10 21:00:52 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -1,3 +1,18 @@
-destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil","Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "So Paulo, Brazil","Cairo, Egypt"]
 
-test_traveller = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+
+def get_destination_index(destination):
+    for index in range(len(destinations)):
+        if destination == destinations[index]:
+            destination_index = index
+            return destination_index
+
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+print(get_traveler_location(test_traveler))
